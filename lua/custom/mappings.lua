2850106fa9f322066f9cf77809901852b9d665a1
Frank <frank@void0.ch>
2024-03-18 20:42:24 +0100

    Add custom remaps

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -4,7 +4,14 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-
+   
+    ["<C-d>"] = { "<C-d>zz", "Scroll down and center" },
+    ["<C-u>"] = { "<C-u>zz", "Scroll up and center" },
+    ["n"] = { "nzzzv", "Search next and center"},
+    ["N"] = { "Nzzzv", "Search back and center"},
+    ["<leader>p"] = { "\"_dP", "Dont yank into paste register"},
+  
+  
     --  format with conform
     ["<leader>fm"] = {
       function()
